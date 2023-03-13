e0a95e05144ecd96c0071f208b8b9d7e78a0e839
Khien <Khienpc2@smartosc.com>
2023-03-13 09:59:32 +0700

    material compoment

---
+++ src/components/common/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <va-navbar
+      color="#282F69"
+      style="height: 100px;"
+  >
+    <template #left>
+      <va-navbar-item class="logo">
+        LOGO
+      </va-navbar-item>
+    </template>
+    <template #right>
+      <va-button color="warning" class="mr-5"> Dashboard </va-button>
+      <va-button color="warning" class="mr-5"> Reports </va-button>
+      <va-button color="warning" class="mr-5"> Users </va-button>
+    </template>
+  </va-navbar>
+</template>
+
+<style scoped>
+.logo {
+  font-weight: 600;
+  font-size: 1.5rem;
+}
+</style>
