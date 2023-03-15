efc6f17cae77e51776d54bef9555cec4cff74d50
Khien <Khienpc2@smartosc.com>
2023-03-15 08:17:42 +0700

    Fetch posts

--- src/containers/Contact.vue
+++ src/containers/Contact.vue
@@ -1,7 +1,21 @@
-<script setup lang="ts">
-
+<script>
+export default {
+  data() {
+    return {
+      posts: [],
+    }
+  },
+  created() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(response => response.json())
+        .then(json => this.posts = json)
+  }
+}
 </script>
 
 <template>
   <h1>Contact</h1>
+  <template v-for="(item, index) in posts" :key="item.id">
+    <div>{{ index }} : {{ item.title }}</div>
+  </template>
 </template>
